a9f6eca982d50ae458e9f03b345f9c604a25e2f5
청현 <eastspring1979@gmail.com>
2026-08-22 14:10:04 +0900

    feat: show admin sheet status by tab color

--- js/groupReview.js
+++ js/groupReview.js
@@ -1,10 +1,12 @@
 import { initGroupReviewV2 } from "./groupReviewV2.js";
 import { installGroupReviewReuseV2 } from "./groupReviewV2Reuse.js";
 import { installGroupReviewRefreshV2 } from "./groupReviewV2Refresh.js";
+import { installGroupReviewAdminTabStatusV2 } from "./groupReviewV2AdminTabStatus.js";
 
 export function initGroupReview() {
   const api = initGroupReviewV2();
   installGroupReviewReuseV2(api);
   installGroupReviewRefreshV2(api);
+  installGroupReviewAdminTabStatusV2();
   return api;
 }
