646aede5dfd82113d8620602ccfb4faf4ba6de74
청현 <eastspring1979@gmail.com>
2026-08-22 14:38:56 +0900

    feat: enable Group Review cell edit locks

--- js/groupReview.js
+++ js/groupReview.js
@@ -2,11 +2,13 @@ import { initGroupReviewV2 } from "./groupReviewV2.js";
 import { installGroupReviewReuseV2 } from "./groupReviewV2Reuse.js";
 import { installGroupReviewRefreshV2 } from "./groupReviewV2Refresh.js";
 import { installGroupReviewAdminTabStatusV2 } from "./groupReviewV2AdminTabStatus.js";
+import { installGroupReviewCellLockV2 } from "./groupReviewV2CellLock.js";
 
 export function initGroupReview() {
   const api = initGroupReviewV2();
   installGroupReviewReuseV2(api);
   installGroupReviewRefreshV2(api);
   installGroupReviewAdminTabStatusV2();
+  installGroupReviewCellLockV2();
   return api;
 }
