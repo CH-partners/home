eb90ebe0afc91a2166705fa0f468e7bd1a55cfe8
청현 <eastspring1979@gmail.com>
2026-08-22 13:42:36 +0900

    feat: wire Group Review V2 reuse UI

--- js/groupReview.js
+++ js/groupReview.js
@@ -1,1 +1,8 @@
-export { initGroupReviewV2 as initGroupReview } from "./groupReviewV2.js";
+import { initGroupReviewV2 } from "./groupReviewV2.js";
+import { installGroupReviewReuseV2 } from "./groupReviewV2Reuse.js";
+
+export function initGroupReview() {
+  const api = initGroupReviewV2();
+  installGroupReviewReuseV2(api);
+  return api;
+}
