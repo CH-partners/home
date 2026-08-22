5148ec65204f7a214941c9bf6278de6758f03ff6
청현 <eastspring1979@gmail.com>
2026-08-22 13:58:53 +0900

    feat: wire in-place Group Review refresh

--- js/groupReview.js
+++ js/groupReview.js
@@ -1,8 +1,10 @@
 import { initGroupReviewV2 } from "./groupReviewV2.js";
 import { installGroupReviewReuseV2 } from "./groupReviewV2Reuse.js";
+import { installGroupReviewRefreshV2 } from "./groupReviewV2Refresh.js";
 
 export function initGroupReview() {
   const api = initGroupReviewV2();
   installGroupReviewReuseV2(api);
+  installGroupReviewRefreshV2(api);
   return api;
 }
