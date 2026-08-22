ae9d82feca3f8e574993dba52298e41daaaf507e
청현 <eastspring1979@gmail.com>
2026-08-22 15:40:23 +0900

    feat: enable limited deployment menu

--- js/groupReview.js
+++ js/groupReview.js
@@ -1,3 +1,4 @@
+import { installLimitedDeploymentMode } from "./deploymentMode.js";
 import { initGroupReviewV2 } from "./groupReviewV2.js";
 import { installGroupReviewReuseV2 } from "./groupReviewV2Reuse.js";
 import { installGroupReviewRefreshV2 } from "./groupReviewV2Refresh.js";
@@ -6,6 +7,7 @@ import { installGroupReviewCellLockV2 } from "./groupReviewV2CellLock.js";
 import { installGroupReviewAdminNavigationV2 } from "./groupReviewV2AdminNavigation.js";
 
 export function initGroupReview() {
+  installLimitedDeploymentMode();
   const api = initGroupReviewV2();
   installGroupReviewReuseV2(api);
   installGroupReviewRefreshV2(api);
