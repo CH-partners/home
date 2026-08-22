b54996331859f6aa85e4d93c93986c8dd20d2d10
청현 <eastspring1979@gmail.com>
2026-08-22 15:14:57 +0900

    feat: integrate Group Review revision workflow

--- js/groupReview.js
+++ js/groupReview.js
@@ -3,6 +3,7 @@ import { installGroupReviewReuseV2 } from "./groupReviewV2Reuse.js";
 import { installGroupReviewRefreshV2 } from "./groupReviewV2Refresh.js";
 import { installGroupReviewAdminTabStatusV2 } from "./groupReviewV2AdminTabStatus.js";
 import { installGroupReviewCellLockV2 } from "./groupReviewV2CellLock.js";
+import { installGroupReviewRevisionV2 } from "./groupReviewV2Revision.js";
 
 export function initGroupReview() {
   const api = initGroupReviewV2();
@@ -10,5 +11,6 @@ export function initGroupReview() {
   installGroupReviewRefreshV2(api);
   installGroupReviewAdminTabStatusV2();
   installGroupReviewCellLockV2();
+  installGroupReviewRevisionV2(api);
   return api;
 }
