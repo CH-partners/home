d37f2b1fb6bbf5df491fbfb41ee7af0f36ce2603
청현 <eastspring1979@gmail.com>
2026-08-23 14:04:51 +0900

    load local notice admin controls

--- js/deploymentMode.js
+++ js/deploymentMode.js
@@ -1,4 +1,5 @@
 import "./sharedPagesLocal.js";
+import "./noticeLocalAdmin.js";
 
 const ALLOWED_PANEL_INDEXES = new Set([0, 10, 11, 13]);
 const ALLOWED_LABELS = new Set(["청현공지사항", "공지사항", "일반게시판", "소액조회", "분배표", "그룹리뷰"]);
