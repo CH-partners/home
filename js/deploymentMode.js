65f781cf0b6b81a783e2d558507440bd5602e119
청현 <eastspring1979@gmail.com>
2026-08-24 08:28:50 +0900

    load sidebar session controls

--- js/deploymentMode.js
+++ js/deploymentMode.js
@@ -4,6 +4,7 @@ import "./legacyBoardsLocal.js";
 import "./fixedBoardMenuCompat.js";
 import "./menuAdminUnified.js";
 import "./menuPillStyle.js";
+import "./sidebarSession.js";
 
 const ALLOWED_PANEL_INDEXES = new Set([0, 10, 11, 13]);
 const ALLOWED_LABELS = new Set(["청현공지사항", "공지사항", "일반게시판", "소액조회", "분배표", "그룹리뷰"]);
