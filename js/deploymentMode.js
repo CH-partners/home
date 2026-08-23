3c79a2d2963e197f02c4010cf53272bf8ee668fa
청현 <eastspring1979@gmail.com>
2026-08-23 17:54:46 +0900

    load pill shaped sidebar menu style

--- js/deploymentMode.js
+++ js/deploymentMode.js
@@ -3,6 +3,7 @@ import "./noticeLocalAdmin.js";
 import "./legacyBoardsLocal.js";
 import "./fixedBoardMenuCompat.js";
 import "./menuAdminUnified.js";
+import "./menuPillStyle.js";
 
 const ALLOWED_PANEL_INDEXES = new Set([0, 10, 11, 13]);
 const ALLOWED_LABELS = new Set(["청현공지사항", "공지사항", "일반게시판", "소액조회", "분배표", "그룹리뷰"]);
